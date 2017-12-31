5126f28333e767c647f29d0b6c550080405045ce
craig91 <craigdejean1@gmail.com>
2017-12-31 11:14:09 -0500

    Added additional classes

--- C#/Grades/GradeBook.cs
+++ C#/Grades/GradeBook.cs
@@ -8,11 +8,34 @@ namespace Grades
 {
     class GradeBook
     {
+        public GradeBook()
+        {
+            grades = new List<float>();        
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            
+
+            float sum = 0;
+            foreach(float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+            stats.AverageGrade = sum / grades.Count;
+
+            return stats;
+        }
+
+
         public void AddGrade(float grade)
         {
             grades.Add(grade);
         }
 
-        List<float> grades; // list convention: names are lower case.
+         private List<float> grades; // list convention: names are lower case.
     }
 }
