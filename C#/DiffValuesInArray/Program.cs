c7a9f458eeb67c4cbe8c4fd2a5d9f8e3586d77a4
craig91 <craigdejean1@gmail.com>
2018-01-03 15:12:32 -0500

    Can you store different types in an array in C# part 1

---
+++ C#/DiffValuesInArray/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiffValuesInArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            object[] array = new object[3]; 
+            array[0] = 1; // Integer
+            array[1] = "string"; // string
+
+            Customer c = new Customer();
+            c.ID = 99;
+            c.Name = "Craig";
+            array[2] = c;   // Customer - Complex type
+
+            foreach(object obj in array)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+
+    class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
