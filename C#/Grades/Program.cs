372421aa2d11548e5276922b1f942403e8ce0bef
craig91 <craigdejean1@gmail.com>
2018-01-05 14:47:40 -0500

    Validation. References to validation in Program.cs

--- C#/Grades/Program.cs
+++ C#/Grades/Program.cs
@@ -12,12 +12,14 @@ namespace Grades
         {
 
             GradeBook book = new GradeBook();
-
+            book.Name = "Craig's grade book";
+            book.Name = null;
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
 
             GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(book.Name);
             WriteResult("Average", stats.AverageGrade);
             WriteResult("Highest", (int)stats.HighestGrade);
             WriteResult("Lowest", stats.LowestGrade); 
