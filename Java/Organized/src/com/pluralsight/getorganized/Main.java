52c79ab8504f9dab99a8a93280e6ac5d6f72ec6a
craig91 <craigdejean1@gmail.com>
2018-01-04 23:28:06 -0500

    updated code: practicing with primitive types

--- Java/Organized/src/com/pluralsight/getorganized/Main.java
+++ Java/Organized/src/com/pluralsight/getorganized/Main.java
@@ -3,6 +3,29 @@ package com.pluralsight.getorganized;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello Get Organized");
+        int myVar;
+
+        char letter;
+
+        letter = 'A';
+
+
+        myVar = 50;
+        System.out.println(myVar);
+
+        int anotherVar = 100;
+        System.out.println(anotherVar);
+
+        myVar = anotherVar;
+        System.out.println(myVar);
+
+        anotherVar = 200;
+        System.out.println(anotherVar);
+
+        System.out.println(myVar);
+
+        System.out.println(letter);
+
+
     }
 }
