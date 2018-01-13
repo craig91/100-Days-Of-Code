e33f4a1ab78e3448fad84fcc336c60dc69cccaba
craig91 <craigdejean1@gmail.com>
2018-01-13 13:05:22 -0500

    Added Switch case statement for grades

--- C#/Grades/GradeStatistics.cs
+++ C#/Grades/GradeStatistics.cs
@@ -15,6 +15,33 @@ namespace Grades
 
         }
 
+        public string Description
+        {
+            get
+            {
+                string result;
+                switch (LetterGrade)
+                {
+                    case "A":
+                        result = "Excellent";
+                        break;
+                    case "B":
+                        result = "Good";
+                        break;
+                    case "C":
+                        result = "Average";
+                        break;
+                    case "D":
+                        result = "Below Average";
+                        break;
+                    default:
+                        result = "failing";
+                        break;
+                }
+                return result;
+            }
+        }
+
        public string LetterGrade
         {
             get
