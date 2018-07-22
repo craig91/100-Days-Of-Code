707774dbe5daaa5598a83975cb31a337e5256a31
craig91 <craigdejean1@gmail.com>
2018-07-22 16:21:00 -0400

    Simple http server in java

---
+++ Java/SimpleHTTPServer/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection port 8080 ....");
+
+        while (true) {
+            try (Socket socket = server.accept()) {
+                Date today = new Date();
+                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+            }
+        }
+    }
+}
