827b53e967eb27fa18fe9fdcbaecebc842041708
craig91 <craigdejean1@gmail.com>
2018-07-23 12:56:38 -0400

    Java Basics revisited

---
+++ Java/control-flow/src/examples/ifElseExample.java
@@ -0,0 +1,12 @@
+package examples;
+
+public class ifElseExample {
+    public static void main(String[] args) {
+        int number = 5;
+
+        if (number % 2 == 0)
+            System.out.println("The number " + number + " is even");
+        else
+            System.out.println("The number " + number + " is odd");
+    }
+}
