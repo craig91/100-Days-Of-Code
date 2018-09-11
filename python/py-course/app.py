480c6459a56571c40bd57514872908c2e6d3a362
Craig Dejean <tachik0ma@localhost.localdomain>
2018-09-11 16:57:43 -0400

    test python file

---
+++ python/py-course/app.py
@@ -0,0 +1,9 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+
+
+#getting input from a user is by default converted into a string
+
+result = float(num1) + float(num2)
+
+print(result)
